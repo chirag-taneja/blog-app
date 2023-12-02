ebcae7d36ca1e5a7d55de73e0d08edf7e0eef8b6
chirag taneja <chirag2000.ct@gmail.com>
2023-12-02 13:35:44 +0530

    remove import

--- src/main/java/com/blog/dao/PostRepo.java
+++ src/main/java/com/blog/dao/PostRepo.java
@@ -1,11 +1,8 @@
 package com.blog.dao;
 
-import com.blog.dto.CustomDto;
 import com.blog.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface PostRepo extends JpaRepository<Post,Long> {
